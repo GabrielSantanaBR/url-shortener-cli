b47127b9f2ded92bc92644438813d22eb608cf1e
GabrielSantanaBR <gabrielbsantana61@gmail.com>
2026-04-29 21:04:06 -0300

    feat: implement JSON storage for URLs

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import os
 import random
 import string
+import json
+
+def load_urls():
+    try:
+        with open("urls.json", "r") as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return{}
+
+def save_urls(data):
+    with open("urls.json", "w") as file:
+        json.dump(data, file, indent = 4)
+
 
 def generate_short_code(length = 6):
     characters = string.ascii_letters + string.digits
@@ -25,6 +38,8 @@ def show_menu():
     => 
     """)
 
+urls = load_urls()
+
 while True:
     clear_screen()
     show_menu()
@@ -32,9 +47,18 @@ while True:
     option = input("Choose an option: ")
 
     if option == "1":
+        clear_screen()
+
+        original_url = input("Enter URL: ")
+
         code = generate_short_code()
+
+        urls[code] = original_url
+        save_urls(urls)
+
         print(f"Generated code: {code}")
         input("Press Enter to continue...")
+
     elif option == "2":
         print("List URLs")
         input("Press Enter to continue...")
